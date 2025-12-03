83cdec706841979906e552f08e85251fb54008a7
dinosancet <94.dino@web.de>
2025-12-03 23:55:30 +0100

    Create Home component with centered text

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <main
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "4rem",
+        fontWeight: "bold",
+      }}
+    >
+      ZAIRE
+    </main>
+  );
+}
