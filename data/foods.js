d919d6597a5cbb050aade2ce9cfacd5c2dd7a2ca
dinosancet <94.dino@web.de>
2025-12-16 21:56:09 +0100

    Refactor foods export and remove unused entries

--- data/foods.js
+++ data/foods.js
@@ -1,4 +1,4 @@
-export const foods = [
+const foods = [
   {
     name: "Banane",
     calories: 89,
@@ -16,49 +16,9 @@ export const foods = [
       { name: "Magnesium", amount: "27 mg", daily: "7%", benefit: "Muskel- und Nervenfunktion" }
     ],
     aminoacids: [
-      { name: "Leucin", amount: "70 mg", daily: "-", benefit: "Muskelaufbau" },
-      { name: "Lysin", amount: "50 mg", daily: "-", benefit: "Immunsystem" }
-    ]
-  },
-
-  {
-    name: "Avocado",
-    calories: 160,
-    macros: {
-      protein: 2,
-      fat: 15,
-      carbs: 9
-    },
-    vitamins: [
-      { name: "Vitamin K", amount: "21 µg", daily: "18%", benefit: "Blutgerinnung" },
-      { name: "Vitamin E", amount: "2.1 mg", daily: "14%", benefit: "Zellschutz" }
-    ],
-    minerals: [
-      { name: "Kalium", amount: "485 mg", daily: "24%", benefit: "Herzgesundheit" },
-      { name: "Kupfer", amount: "0.2 mg", daily: "22%", benefit: "Energieproduktion" }
-    ],
-    aminoacids: [
-      { name: "Tryptophan", amount: "30 mg", daily: "-", benefit: "Serotonin-Bildung" }
-    ]
-  },
-
-  {
-    name: "Brokkoli",
-    calories: 34,
-    macros: {
-      protein: 2.8,
-      fat: 0.4,
-      carbs: 7
-    },
-    vitamins: [
-      { name: "Vitamin C", amount: "89 mg", daily: "98%", benefit: "Starkes Immunsystem" },
-      { name: "Vitamin K", amount: "101 µg", daily: "84%", benefit: "Knochenstärke" }
-    ],
-    minerals: [
-      { name: "Kalzium", amount: "47 mg", daily: "5%", benefit: "Knochen & Zähne" }
-    ],
-    aminoacids: [
-      { name: "Leucin", amount: "50 mg", daily: "-", benefit: "Muskeln" }
+      { name: "Leucin", amount: "70 mg", daily: "-", benefit: "Muskelaufbau" }
     ]
   }
-];
+]
+
+export default foods
